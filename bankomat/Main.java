c7f6eaf4f4e131993859811edd7c1ba4face1ce4
georgiibrazhnikov-dev <georgiibrazhnikov@gmail.com>
2026-05-20 23:21:14 +0300

    Delete bankomat directory

--- bankomat/Main.java
+++
@@ -1,9 +0,0 @@
-package bankomat;
-
-public class Main {
-    public static void main(String[] args) {
-        BankAccount account = new BankAccount("Georgii Brazhnikov", 10000);
-        ATM atm = new ATM(account);
-        atm.showMenu();
-    }
-}  
